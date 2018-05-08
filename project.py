9e21a5ee9afe087846b589053895753f64e78f04
progresivoJS <progresivoJS@gmail.com>
2018-05-09 04:32:08 +0900

    feat: implement routing function of create, edit, delete menu item

--- project.py
+++ project.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
-def restaurantMenu(restaurant_id):
+def restaurantMenu(restaurant_id): # should match with paramter of url accordingly.
     items = restaurant_dao.get_menu(restaurant_id)
 
     output = ''
@@ -15,6 +15,18 @@ def restaurantMenu(restaurant_id):
         output += item.description + "</br>"
         output += "</br>"
     return output
+
+@app.route('/restaurants/<int:restaurant_id>/new/')
+def newMenuItem(restaurant_id):
+    pass
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
+def editMenuItem(restaurant_id, menu_id):
+    pass
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
+def deleteMenuItem(restaurant_id, menu_id):
+    pass
 
 if __name__ == '__main__':
     app.debug = True
