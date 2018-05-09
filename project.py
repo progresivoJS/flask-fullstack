634e7131953ffba0940cf57eae9f3d65320dbec2
progresivoJS <progresivoJS@gmail.com>
2018-05-10 04:18:43 +0900

    feat: add get request using jsonify

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect, flash
+from flask import Flask, render_template, url_for, request, redirect, flash, jsonify
 import restaurant_dao 
 
 app = Flask(__name__)
@@ -40,6 +40,17 @@ def deleteMenuItem(restaurant_id, menu_id):
         restaurant_dao.delete_item_from_restaurant(restaurant_id, menu_id)
         flash("Menu Item Deleted")
         return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+
+# Making an API Endpoint (GET Request)
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON/')
+def restaurantMenuJSON(restaurant_id):
+    items = restaurant_dao.get_menus_of_restaurant(restaurant_id)
+    return jsonify(MenuItems = [i.serialize for i in items])
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON/')
+def get_menu(restaurant_id, menu_id):
+    item = restaurant_dao.get_menu(menu_id)
+    return jsonify(MenuItem = item.serialize)
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
